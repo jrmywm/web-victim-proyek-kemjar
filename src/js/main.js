bcd441a4d02b9841a58f11ddaa0cb5c23aa5df75
jrmywm <jimmywm@hotmail.co.uk>
2025-12-05 17:45:35 +0700

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -5,8 +5,6 @@ import { initLoginButtons } from './components/loginButtons.js';
 import { initWebsiteButton } from './components/websiteButton.js';
 import { initFooterLinks } from './components/footerLinks.js';
 import { utils } from './utils/dateTime.js';
-import { Analytics } from "@vercel/analytics/next"
-
 // Initialize all components when DOM is ready
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Bank Nusantara - Internet Banking initialized');
